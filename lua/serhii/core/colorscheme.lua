31ab7e309b8cdac3a452dfc6b98a05a07f0ded6d
Serhii Budnik <serhiib@scribd.com>
2023-04-22 13:56:11 +0300

    fix theme

--- lua/serhii/core/colorscheme.lua
+++ lua/serhii/core/colorscheme.lua
@@ -5,38 +5,32 @@ local darkBlue = '#001080'
 local constColor = '#257f99'
 
 require('vscode').setup({
-    -- Alternatively set style in setup
-    -- style = 'light'
+  -- Enable italic comment
+  italic_comments = true,
 
-    -- Enable transparent background
-    transparent = true,
+  -- Disable nvim-tree background color
+  disable_nvimtree_bg = false,
 
-    -- Enable italic comment
-    italic_comments = true,
+  -- Override colors (see ./lua/vscode/colors.lua)
+  color_overrides = {
+    vscLineNumber = '#6E7681',
+  },
 
-    -- Disable nvim-tree background color
-    disable_nvimtree_bg = true,
+  -- Override highlight groups (see ./lua/vscode/theme.lua)
+  group_overrides = {
+    -- this supports the same val table as vim.api.nvim_set_hl
+    -- use colors from this colorscheme by requiring vscode.colors!
+    -- Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
 
-    -- Override colors (see ./lua/vscode/colors.lua)
-    color_overrides = {
-        vscLineNumber = '#6E7681',
-    },
+    ["@symbol.ruby"] = { fg = c.vscBlue, bg = "NONE" },
+    ["@variable.ruby"] = { fg = darkBlue, bg = "NONE" },
+    -- instance & class variables
+    ["@label.ruby"] = { fg = darkBlue, bg = "NONE" },
+    ["@keyword.function.ruby"] = { fg = c.vscPink, bg = "NONE" },
+    ["@type.ruby"] = { fg = constColor, bg = "NONE" },
 
-    -- Override highlight groups (see ./lua/vscode/theme.lua)
-    group_overrides = {
-      -- this supports the same val table as vim.api.nvim_set_hl
-      -- use colors from this colorscheme by requiring vscode.colors!
-      -- Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-
-      ["@symbol.ruby"] = { fg = c.vscBlue, bg = "NONE" },
-      ["@variable.ruby"] = { fg = darkBlue, bg = "NONE" },
-      -- instance & class variables
-      ["@label.ruby"] = { fg = darkBlue, bg = "NONE" },
-      ["@keyword.function.ruby"] = { fg = c.vscPink, bg = "NONE" },
-      ["@type.ruby"] = { fg = constColor, bg = "NONE" },
-
-      ["@string"] = { fg = '#a31414', bg = "NONE" },
-      ["@string.regex"] = { fg = c.vscYellowOrange, bg = "NONE" },
-    }
+    ["@string"] = { fg = '#a31414', bg = "NONE" },
+    ["@string.regex"] = { fg = c.vscYellowOrange, bg = "NONE" },
+  }
 })
 require('vscode').load()
