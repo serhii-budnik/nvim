a32dcba8bc4219e61594752713e1b7c64098d8ae
Serhii Budnik <serhiib@scribd.com>
2023-05-16 22:03:44 +0300

    testing <leader>fp mapping for find files

--- lua/serhii/core/keymaps.lua
+++ lua/serhii/core/keymaps.lua
@@ -23,7 +23,7 @@ keymap.set("n", '<leader>ff', builtin.git_files, {})
 keymap.set("n", '<leader>fo', function()
   builtin.oldfiles({ only_cwd = true })
 end, {})
--- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fp", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
