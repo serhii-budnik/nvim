4bc3ff38a21a8ca679977054d7b69f3a6a879917
Serhii Budnik <serhiib@scribd.com>
2023-05-27 18:57:44 +0300

    add luasnip configuration

---
+++ lua/serhii/plugins/luasnip.lua
@@ -0,0 +1,15 @@
+local ls = require "luasnip"
+
+require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets"})
+
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true })
---
+++ snippets/ruby.lua
@@ -0,0 +1,28 @@
+local ls = require 'luasnip'
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local rep = require('luasnip.extras').rep
+
+ls.add_snippets('ruby', {
+  s(
+    'rlog',
+    {
+      t('Rails.logger.info("'),
+      i(1, 'var'),
+      t('>>>>>>> #{'),
+      rep(1),
+      t('.inspect}")'),
+      t({'', ''})
+    }
+  ),
+  s(
+    'fro',
+    {
+      t('# frozen_string_literal: true'),
+      t({'', ''}),
+      t({'', ''}),
+    }
+  )
+})
